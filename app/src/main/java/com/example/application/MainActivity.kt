5cdf9689d85af33085175fbce4e61e478f7dd39c
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-06 22:45:02 +0530

    logo added

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -20,7 +23,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.application.ui.theme.ApplicationTheme
@@ -50,6 +55,12 @@ fun MainScreen(){
             .fillMaxSize()
             .padding(bottom = 40.dp),
             verticalArrangement = Arrangement.Bottom){
+
+            Box(modifier = Modifier.fillMaxWidth().offset(y = -199.dp)){
+                Image(painter = painterResource(id = R.drawable.logo),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth())
+            }
 
             Row(modifier = Modifier
                 .fillMaxWidth(),
