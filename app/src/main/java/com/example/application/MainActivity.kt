7f91a570d11ce5d9592d5d87b1d2075eee904205
HashSuranjana <hasithasuranjana12@gmail.com>
2024-04-23 11:00:15 +0530

    updated files

--- app/src/main/java/com/example/application/MainActivity.kt
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -5,16 +5,12 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -23,10 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.application.ui.theme.ApplicationTheme
@@ -56,11 +49,11 @@ class MainActivity : ComponentActivity() {
                 .padding(bottom = 40.dp),
                 verticalArrangement = Arrangement.Bottom){
 
-                Box(modifier = Modifier.fillMaxWidth().offset(y = -199.dp)){
-                    Image(painter = painterResource(id = R.drawable.logo),
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth())
-                }
+//                Box(modifier = Modifier.fillMaxWidth().offset(y = (-199).dp)){
+//                    Image(painter = painterResource(id = R.drawable.logo),
+//                        contentDescription = null,
+//                        modifier = Modifier.fillMaxWidth())
+//                }
 
                 Row(modifier = Modifier
                     .fillMaxWidth(),
@@ -100,47 +93,44 @@ class MainActivity : ComponentActivity() {
         else {
             Row(modifier = Modifier
                 .fillMaxSize()
-                .padding(bottom = 40.dp),
-                verticalAlignment = Alignment.Bottom){
+                .padding(end = 20.dp),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically){
 
-                Box(modifier = Modifier.fillMaxWidth().offset(y = -199.dp)){
-                    Image(painter = painterResource(id = R.drawable.logo),
-                        contentDescription = null,
-                        modifier = Modifier.fillMaxWidth())
-                }
+//                Box(modifier = Modifier.offset(x = (-280).dp)){
+//                    Image(painter = painterResource(id = R.drawable.logo),
+//                        contentDescription = null,
+//                        modifier = Modifier.fillMaxWidth())
+//                }
 
-                Column(modifier = Modifier
-                    .fillMaxWidth(),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally){
+                Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround) {
+                    Row(horizontalArrangement = Arrangement.SpaceAround,
+                        verticalAlignment = Alignment.CenterVertically){
 
-                    Button(onClick = {
-                        val intent = Intent(this@MainActivity,AddActivity::class.java)
-                        startActivity(intent)
-                    }) {
-                        Text(text = "Add Leagues to DB")
+                        Button(onClick = {
+                            val intent = Intent(this@MainActivity,AddActivity::class.java)
+                            startActivity(intent)
+                        }) {
+                            Text(text = "Add Leagues to DB")
 
-                    }
+                        }
 
-                    Button(onClick = { }) {
-                        Text(text = "Search for clubs by League")
+                        Button(onClick = { }) {
+                            Text(text = "Search for clubs by League")
 
-                    }
-
-                }
-
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .height(80.dp),
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally){
-
-                    Button(onClick = { }) {
-                        Text(text = "Search for Clubs")
+                        }
 
                     }
-                }
 
+                    Column(horizontalAlignment = Alignment.Start,
+                        modifier = Modifier.padding(20.dp)) {
+                        Button(onClick = { }) {
+                            Text(text = "Search for Clubs")
+
+                        }
+                    }
+                }
             }
         }
     }
