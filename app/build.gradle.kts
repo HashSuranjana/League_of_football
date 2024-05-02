e4f6cc4a8f62c321761f3b0211b6aa7c066748c1
HashSuranjana <hasithasuranjana12@gmail.com>
2024-05-02 10:56:40 +0530

    Updated files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+// To use Kotlin Symbol Processing (KSP)
+    ksp("androidx.room:room-compiler:$room_version")
+// optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
 }
